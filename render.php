9b039424ae2bfdab21d2403bd3f257a47ad21dda
Xiangqun Zhang <zhxq@zhxqweb.com>
2023-06-19 02:29:04 -0400

    Test

--- render.php
+++ render.php
@@ -365,19 +365,23 @@ function parse_def($payload, $attrenv, $varenv){
             $tmp = 'class="' . $d . '"';
             if ($d != '') $attrStr .= " $tmp";
         }else{
-            if ($v[0] == '$' && $v[-1] == '$'){
-                $keyname = substr($v, 1, -1);
-                $v = $varenv[$keyname];
-                if ($v == null){
-                    $v = $def['default']['attr'][$keyname];
-                    if ($v[0] == '$' && $v[-1] == '$'){
-                        $v = $varenv[$keyname];
+            $varr = $v;
+            if (gettype($v) == gettype('')){
+                $varr = [$v];
+            }
+            foreach ($varr as $ve){
+                if ($ve[0] == '$' && $ve[-1] == '$'){
+                    $keyname = substr($ve, 1, -1);
+                    $ve = $varenv[$keyname];
+                    if ($ve == null){
+                        $ve = $def['default']['attr'][$keyname];
+                        if ($ve[0] == '$' && $ve[-1] == '$'){
+                            $ve = $varenv[$keyname];
+                        }
                     }
                 }
             }
-            if (gettype($v) == gettype([])){
-                $v = implode('', $v);
-            }
+            $v = implode('', $varr);
             $attrStr .= " $k=\"$v\"";
         }
     }
