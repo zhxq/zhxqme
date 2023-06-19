1ac86846a82d59d19c4c2109d0cdf9668b707626
Xiangqun Zhang <zhxq@zhxqweb.com>
2023-06-19 03:01:54 -0400

    Update

--- definitions/templates/js/picwithexif.js
+++ definitions/templates/js/picwithexif.js
@@ -1,6 +1,16 @@
+function picwithexif_getInvert(num){
+    if (num >= 1){
+        return num;
+    }else if (num > 0){
+        return "1/" + Math.round((1 / num));
+    }else{
+        return num;
+    }
+}
+
 function picwithexif_call(id){
     EXIF.getData(document.getElementById(id + '_pic'), function(){
-        var tags = EXIF.getTag(this, "Make") + ' ' + EXIF.getTag(this, "Model") + ' ' + EXIF.getTag(this, "FocalLength") + "mm " + EXIF.getTag(this, "ExposureTime") + ' f/' + EXIF.getTag(this, "FNumber") + ' ' + EXIF.getTag(this, "ApertureValue");
+        var tags = EXIF.getTag(this, "Make") + ' ' + EXIF.getTag(this, "Model") + ' ' + EXIF.getTag(this, "FocalLength") + "mm " + picwithexif_getInvert(EXIF.getTag(this, "ExposureTime")) + 's f/' + EXIF.getTag(this, "FNumber") + ' ' + EXIF.getTag(this, "ApertureValue");
         $('#' + id + '_exif').html(tags);
     });
 }
