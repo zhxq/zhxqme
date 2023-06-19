298a3f0076a922689507e09659e7ba33f70cd9e0
Xiangqun Zhang <zhxq@zhxqweb.com>
2023-06-19 02:56:34 -0400

    Update

--- definitions/templates/js/picwithexif.js
+++ definitions/templates/js/picwithexif.js
@@ -1,3 +1,6 @@
 function picwithexif_call(id){
-    EXIF.getData(document.getElementById(id + '_pic'), function(){var tags=EXIF.getTag(this, "Make") + EXIF.getTag(this, "Model") + EXIF.getTag(this, "FocalLength") + EXIF.getTag(this, "ShutterSpeedValue") + EXIF.getTag(this, "FNumber") + EXIF.getTag(this, "ApertureValue");$('#' + id + '_exif').html(tags);});
+    EXIF.getData(document.getElementById(id + '_pic'), function(){
+        var tags = EXIF.getTag(this, "Make") + ' ' + EXIF.getTag(this, "Model") + ' ' + EXIF.getTag(this, "FocalLength") + EXIF.getTag(this, "ShutterSpeedValue") + ' ' + EXIF.getTag(this, "FNumber") + ' ' + EXIF.getTag(this, "ApertureValue");
+        $('#' + id + '_exif').html(tags);
+    });
 }
