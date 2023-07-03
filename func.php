1dd0e93c9f1e1d8582e6c4e69568d929f93771e4
Xiangqun Zhang <zhxq@zhxqweb.com>
2023-07-03 01:52:38 -0400

    Update

--- func.php
+++ func.php
@@ -1,8 +1,8 @@
 <?php
 //For Debug
-ini_set('display_errors', 0);
-ini_set('display_startup_errors', 0);
-// error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 
 $resumate_version = "0.0.1";
