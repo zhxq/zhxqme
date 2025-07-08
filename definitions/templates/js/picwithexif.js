a34fce7d75117d1e914f2cb8e5e49624ef0caa73
Xiangqun Zhang <zhxq@zhxqweb.com>
2025-07-08 02:30:05 -0700

    HDR Compatibility for iOS 26 Onwards
    HDR Compatibility for iOS 26 Onwards

--- definitions/templates/js/picwithexif.js
+++ definitions/templates/js/picwithexif.js
@@ -8,11 +8,28 @@ function picwithexif_getInvert(num){
     }
 }
 
+function appleVersion() {
+    if (
+        /iP(hone|od|ad)/.test(navigator.userAgent) || 
+        navigator.userAgent.includes("Macintosh") || 
+        navigator.userAgent.includes("Mac OS X")
+    ) {
+        var systemVersion = (navigator.userAgent).match(/(\d+)_(\d+)_?(\d+)?/) ?? ['0', '0', '0', '0'];
+        var safariVersion = (navigator.userAgent).match(/Version\/(\d+).(\d+).?(\d+)?/) ?? ['0', '0', '0', '0'];
+        // Some quirks about Safari: may report incorrect OS version but correct Safari version
+        // Found on iPad OS 26 Developer Beta 3 (System=10.15.7, Safari=26.0)
+        return [parseInt(systemVersion[1], 10), parseInt(safariVersion[1], 10)];
+    }
+    return [0, 0];
+}
+
 function picwithexif_call(id){    
     var element = document.getElementById(id + '_pic');
     if (!navigator.userAgent.includes('Chrome') && navigator.userAgent.includes('Safari')) {
-        // Fix Safari false positive
-        $(element.parentElement.firstChild).attr("srcset", "");
+        // Fix Safari false positive when the system is older than iOS/macOS 26
+        if (appleVersion()[0] < 26 && appleVersion()[1] < 26){
+            $(element.parentElement.firstChild).attr("srcset", "");
+        }
     }
     var currentSrc = element.currentSrc;
     var newElmt = document.createElement("img");
